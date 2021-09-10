bec4f8a381e1e98b6274f5f665d7c9473fb2b074
schrulnz <12530037+schrulnz@users.noreply.github.com>
2021-09-10 13:13:06 +0200

    sql part 02 and database

--- shared/src/commonMain/kotlin/com/example/justdesserts/shared/ApolloProvider.kt
+++ shared/src/commonMain/kotlin/com/example/justdesserts/shared/ApolloProvider.kt
@@ -1,16 +1,30 @@
 package com.example.justdesserts.shared
 
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.interceptor.BearerTokenInterceptor
+import com.apollographql.apollo.interceptor.TokenProvider
 import com.apollographql.apollo.network.http.ApolloHttpNetworkTransport
+import com.example.justdesserts.shared.cache.Database
+import com.example.justdesserts.shared.cache.DatabaseDriverFactory
 
-class ApolloProvider {
+class ApolloProvider(databaseDriverFactory: DatabaseDriverFactory) : TokenProvider {
+    internal val database = Database(databaseDriverFactory)
     internal val apolloClient: ApolloClient = ApolloClient(
         networkTransport = ApolloHttpNetworkTransport(
             serverUrl = "https://ktor-graphqul-server.herokuapp.com",
             headers = mapOf(
                 "Accept" to "application/json",
                 "Content-Type" to "application/json"
-            )
-        )
+            ),
+        ),
+        interceptors = listOf(BearerTokenInterceptor(this))
     )
+
+    override suspend fun currentToken(): String {
+        return database.getUserState()?.token ?: ""
+    }
+
+    override suspend fun refreshToken(previousToken: String): String {
+        return ""
+    }
 }
---
+++ shared/src/commonMain/kotlin/com/example/justdesserts/shared/cache/Database.kt
@@ -0,0 +1,85 @@
+package com.example.justdesserts.shared.cache
+
+class Database(databaseDriverFactory: DatabaseDriverFactory) {
+    private val database = JustDesserts(databaseDriverFactory.createDriver())
+    private val dbQuery = database.justDessertsQueries
+
+    internal fun clearDatabase() {
+        dbQuery.transaction {
+            dbQuery.removeAllDesserts()
+        }
+    }
+
+    internal fun getUserState(): UserState? {
+        return dbQuery.selectUserState().executeAsOneOrNull()
+    }
+
+    internal fun saveUserState(userId: String, token: String) {
+        dbQuery.transaction {
+            insertUserState(userId, token)
+        }
+    }
+
+    private fun insertUserState(userId: String, token: String) {
+        dbQuery.insertUserState(userId, token)
+    }
+
+    internal fun deleteUserState() {
+        dbQuery.transaction {
+            removeUserState()
+        }
+    }
+
+    private fun removeUserState() {
+        dbQuery.removeUserState()
+    }
+
+    internal fun getDesserts(): List<Dessert> {
+        return dbQuery.selectAllDesserts().executeAsList()
+    }
+
+    internal fun getDessertById(desserId: String): Dessert? {
+        return dbQuery.selectDessertById(desserId).executeAsOneOrNull()
+    }
+
+    internal fun saveDessert(dessert: Dessert) {
+        dbQuery.transaction {
+            insertDessert(dessert)
+        }
+    }
+
+    internal fun deleteDessert(dessertId: String) {
+        dbQuery.transaction {
+            removeDessert(dessertId)
+        }
+    }
+
+    internal fun updateDessert(dessert: Dessert) {
+        dbQuery.transaction {
+            updateDessertById(dessert)
+        }
+    }
+
+    private fun removeDessert(dessertId: String) {
+        dbQuery.removeDessertById(dessertId)
+    }
+
+    private fun updateDessertById(dessert: Dessert) {
+        dbQuery.updateDessertById(
+            name = dessert.name,
+            description = dessert.description,
+            imageUrl = dessert.imageUrl,
+            id = dessert.id
+        )
+    }
+
+    private fun insertDessert(dessert: Dessert) {
+        dbQuery.insertDessert(
+            dessert.id,
+            dessert.userId,
+            dessert.name,
+            dessert.description,
+            dessert.imageUrl,
+        )
+    }
+}
